398c913a0f2437d94520c45c109cdc5935a208e3
Tom Mollitor <32785972+tmollitor@users.noreply.github.com>
2024-04-10 21:17:09 -0400

    End of section 6

--- src/IdentityService/Config.cs
+++ src/IdentityService/Config.cs
@@ -30,6 +30,18 @@ public static class Config
                 ClientSecrets = new[] {new Secret("NotASecret".Sha256())},
                 AllowedGrantTypes = {GrantType.ResourceOwnerPassword}
             },
+            new Client
+            {
+                ClientId = "nextApp",
+                ClientName = "nextApp",
+                ClientSecrets = {new Secret("secret".Sha256())},
+                AllowedGrantTypes = GrantTypes.CodeAndClientCredentials,
+                RequirePkce = false,
+                RedirectUris = {"https://localhost:3000/api/auth/callback/id-server"},
+                AllowOfflineAccess = true,
+                AllowedScopes = {"openid", "profile", "auctionApp"},
+                AccessTokenLifetime = 3600*24*30
+            }
 
             // // m2m client credentials flow client
             // new Client
